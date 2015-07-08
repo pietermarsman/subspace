2c015af57de747280f5e777597070ef3bddd9871
Pieter Marsman <pietermarsman@gmail.com>
2015-07-08 20:22:20 +0200

    Changed duration analyze and experiment: using unlimited reps with HSSC

--- runables/figures/analyze_duration.m
+++ runables/figures/analyze_duration.m
@@ -1,18 +1,36 @@
-clear all;
+clean
 
-name = 'yale_duration87368.mat';
+name = 'duration28924.mat';
 load(['data/', name]);
-dir = 'fig/rep_vs_err';
+dir = 'fig';
+mkdir(dir);
 
 unique_N = sort(unique(subsets * 64));
 avg = [];
 st = [];
+duri = [];
+dur_std = [];
 for N = unique_N
     avg = [avg; mean(err(:, subsets * 64 == N), 2)'];
     st = [st; std(err(:, subsets * 64 == N), 1, 2)'];
+    duri = [duri; mean(dur(:, subsets * 64 == N), 2)'];
+    dur_std = [dur_std; std(dur(:, subsets * 64 == N), 1, 2)'];
 end
 
-plot(repmat(unique_N', 1, length(names)), avg, st)
+errorbar(repmat(unique_N', 1, length(names)), avg, st)
 set(gca, 'XTick', unique_N)
 legend(names)
 beautyplot('N', 'Error', '', false)
+savename = [dir, '/err_vs_N'];
+savefig(savename)
+export_fig(savename, '-pdf', '-transparent')
+
+
+cla;
+errorbar(repmat(unique_N', 1, length(names)), duri, dur_std)
+set(gca, 'XTick', unique_N)
+legend(names)
+beautyplot('N', 'Duration', '', false)
+savename = [dir, '/dur_vs_N'];
+savefig(savename)
+export_fig(savename, '-pdf', '-transparent')
