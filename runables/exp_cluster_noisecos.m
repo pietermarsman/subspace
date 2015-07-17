500ac5dd2b7cb92ddd9374fa285ad7d3fb0b3b25
Pieter Marsman <pietermarsman@gmail.com>
2015-07-17 15:33:10 +0200

    Updated cluster_noisecos; try catch around experiment

--- runables/exp_cluster_noisecos.m
+++ runables/exp_cluster_noisecos.m
@@ -1,10 +1,11 @@
-function savefile = exp_cluster_noisecos( dataset, dataparams, repeats )
+function savefile = exp_cluster_noisecos( dataparams, repeats )
 %EXPERIMENT 
-%   Example use: workspace; clean; savefile=exp_cluster_noisecos({1, 2, 'cars1'}, {}, 2);
-%   dataset = cell array with dataset descriptions, e.g. {1, 2, 'cars1'}
+%   Example use: workspace; clean; savefile=exp_cluster_noisecos({}, 2);
 %   dataparams = description for generation of data (dataset 1)
+%   repeats = number of times to repeat the experiment
 
 %% PARAMS
+dataset = {1}
 verbose = true
 params = {'sAlphas', [5], ...
     'rAlphas', [50], ...
@@ -12,24 +13,23 @@ params = {'sAlphas', [5], ...
     'pReps', [.1] ...
     };
 cosses = [0:.1:1];
-noises = [0:.001:0.01]
+noises = [0:.0001:0.005];
 
 %% SETUP
-[savefile] = setup_save(['clustering_param', num2str(round(rand() * 100000))]);
+[savefile] = setup_save(['cluster_noisecos', num2str(round(rand() * 100000))]);
 
 %% EXPERIMENT
 fprintf('==%s==', savefile)
-dataset = repmat(dataset, 1, repeats);
-for i = 1:length(dataset)
-    for cos = cosses
-        for noise = noises
-            dataparams = {dataparams{:}, 'cos', cos, 'noise', noise}
-            [ x, labels, N, d, n, D, noise, cos ] = get_data(dataset{i}, dataparams{:});
-            fprintf('\nExperiment %d/%d with N=%d, n=%d, d=%d, D=%d and noise=%s\n> ', ...
-                i, length(dataset), N, n, d, D, noise)
-            [err(:, i), mut(:, i), dur(:, i), pred{i}, cs{i}, rep{i}, names{i}] = experiment(x, labels, n, params{:});
-        end
-    end
+
+[cosses, noises] = meshgrid(cosses, noises);
+cosses = cosses(:)
+noises = noises(:)
+for i = 1:length(cosses)
+    dataparams = {dataparams{:}, 'cos', cosses(i), 'noise', noises(i)};
+    [ x, labels, N, d, n, D, noise, cos ] = get_data(dataset{1}, dataparams{:});
+    fprintf('\nExperiment %d/%d with N=%d, n=%d, d=%d, D=%d, cos=%d and noise=%d\n> ', ...
+        i, length(cosses), N, n, d, D, cos, noise)
+    [err(:, i), mut(:, i), dur(:, i), pred{i}, cs{i}, rep{i}, names{i}] = experiment(x, labels, n, params{:});
 end
 
 %% POST PROCESS
--- utils/exp_dataset.m
+++ utils/exp_dataset.m
@@ -2,8 +2,12 @@ fprintf('==%s==', savefile)
 dataset = repmat(dataset, 1, repeats);
 
 for i = 1:length(dataset)
-    [ x, labels, N, d, n, D, noise, cos ] = get_data(dataset{i}, dataparams{:});
-    fprintf('\nExperiment %d/%d with N=%d, n=%d, d=%d, D=%d and noise=%s\n> ', ...
-        i, length(dataset), N, n, d, D, noise)
-    [err(:, i), mut(:, i), dur(:, i), pred{i}, cs{i}, rep{i}, names{i}] = experiment(x, labels, n, params{:});
+    try
+        [ x, labels, N, d, n, D, noise, cos ] = get_data(dataset{i}, dataparams{:});
+        fprintf('\nExperiment %d/%d with N=%d, n=%d, d=%d, D=%d and noise=%s\n> ', ...
+            i, length(dataset), N, n, d, D, noise)
+        [err(:, i), mut(:, i), dur(:, i), pred{i}, cs{i}, rep{i}, names{i}] = experiment(x, labels, n, params{:});
+    catch ME
+        warning(ME.message)
+    end
 end
