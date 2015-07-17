8b99519b3f3fb732cf43f6a3529f6a8f83767814
Pieter Marsman <pietermarsman@gmail.com>
2015-07-17 16:02:17 +0200

    Filling experiment return values when a crash occurs; increasing default noise

--- utils/exp_dataset.m
+++ utils/exp_dataset.m
@@ -3,11 +3,19 @@ dataset = repmat(dataset, 1, repeats);
 
 for i = 1:length(dataset)
     try
+        error('hello')
         [ x, labels, N, d, n, D, noise, cos ] = get_data(dataset{i}, dataparams{:});
         fprintf('\nExperiment %d/%d with N=%d, n=%d, d=%d, D=%d and noise=%s\n> ', ...
             i, length(dataset), N, n, d, D, noise)
         [err(:, i), mut(:, i), dur(:, i), pred{i}, cs{i}, rep{i}, names{i}] = experiment(x, labels, n, params{:});
     catch ME
         warning(ME.message)
+        err(:, i) = -1;
+        mut(:, i) = -1;
+        dur(:, i) = -1;
+        pred{i} = ME.message;
+        cs{i} = ME.message;
+        rep{i} = ME.message;
+        names{i}{1} = ME.message;
     end
 end
